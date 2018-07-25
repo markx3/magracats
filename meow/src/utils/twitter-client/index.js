c6e56ad4d91f0a7a33ef38f68144eec71c8a6118
Marcos Felipe Eipper <marcos@eipper.com.br>
2018-07-25 00:03:12 -0300

    Testing deploy to heroku.

---
+++ meow/src/utils/twitter-client/index.js
@@ -0,0 +1,35 @@
+"use strict"
+
+class TwitterClient {
+  constructor(track, has, locations) {
+    this.track = track
+    this.has = has
+    this.locations = locations
+    this.keys= {
+      consumer_key: "u61Iy7AXo5tg4cTSWMCjAEHrQ",
+      consumer_secret: "wXI03DoE7g5xWU9Y6q0rVE8M2Gnb4LiCFBvLkJSEBA7mlZeMMU",
+      token: "339830644-RPeqQnXoBrqlDXQ8NaMG8RahZIMmolEla8EHPD6Z",
+      token_secret: "x81KdY3qQyBcV4P47V50nwJ3wzzeWwq5vbi14vamtJNkR"
+    }
+    this.url = "https://api.twitter.com/1.1/search/tweets.json"
+  }
+
+  createStream() {
+    let TwitterStream = require('twitter-stream-api')
+    this.Twitter = new TwitterStream(this.keys, false);
+    this.Twitter.stream('statuses/filter', {
+            track: this.track,
+            has: this.has,
+            locations: this.locations
+          })
+    return this.Twitter
+  }
+}
+
+export default TwitterClient
+
+// let twitterClient = new TwitterClient(['gato', 'adoção', 'gatos'], 'images', '')
+// let stream = twitterClient.createStream()
+// console.log(stream)
+// let fs = require('fs')
+// stream.pipe(fs.createWriteStream('tweets.json'))
